315b048fc523c9248a40e80a286e9a67055f44fc
vennarshulytz <3530163057@qq.com>
2026-01-14 09:10:26 +0800

    feat(project): 初始化 Spring Boot Starter 项目
    添加了基础的 Spring Boot 项目结构，包括以下内容：
    
    - Maven 配置（pom.xml）
    - 应用程序入口类和测试类
    - 配置文件（application.properties）
    - 常见 IDE 配置的 .gitignore 文件

---
+++ src/main/java/io/github/vennarshulytz/jsonviewext/JsonViewExtSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.vennarshulytz.jsonviewext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonViewExtSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonViewExtSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/vennarshulytz/jsonviewext/JsonViewExtSpringBootStarterApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.vennarshulytz.jsonviewext;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JsonViewExtSpringBootStarterApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+
+}
